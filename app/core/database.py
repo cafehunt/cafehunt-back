ba7dd9b0d135e428d1d3f64858c772a92b04d66b
manjustice <manjustice1998@gmail.com>
2023-06-07 18:25:12 +0300

    Added cafe, image, city models and db engine

---
+++ app/core/database.py
@@ -0,0 +1,17 @@
+from typing import Any
+
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+SQLALCHEMY_DATABASE_URL = "sqlite:///./cafe.db"
+
+
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+)
+
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base: Any = declarative_base()

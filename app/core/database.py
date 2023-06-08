76391aaebcda4208cc2e932fa687b7b363f79296
manjustice <manjustice1998@gmail.com>
2023-06-08 15:01:08 +0300

    Moved db connection to asyncio

--- app/core/database.py
+++ app/core/database.py
@@ -1,17 +1,15 @@
 from typing import Any
 
-from sqlalchemy import create_engine
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./cafe.db"
+SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./cafe.db"
 
 
-engine = create_engine(
-    SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
+engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
+
+async_session = sessionmaker(
+    engine, class_=AsyncSession, expire_on_commit=False
 )
-
-SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-
 Base: Any = declarative_base()
--- app/repositories/base.py
+++ app/repositories/base.py
@@ -11,30 +11,30 @@ class BaseRepository:
     def __init__(self, session):
         self.session = session
 
-    def create(self, new_obj: dict):
+    async def create(self, new_obj: dict):
         query = insert(self.model).returning(self.model)
 
-        response = self.session.execute(query, new_obj)
-        self.session.commit()
+        response = await self.session.execute(query, new_obj)
+        await self.session.commit()
         result = response.scalar()
 
         return result
 
-    def get_all(self, query: Select, scalars: bool = True):
-        response = self.session.execute(query)
+    async def get_all(self, query: Select, scalars: bool = True):
+        response = await self.session.execute(query)
 
         result = response.scalars().all() if scalars else response.all()
 
         return result
 
-    def get_one_obj(self, query: Select, scalar: bool = True):
-        response = self.session.execute(query)
+    async def get_one_obj(self, query: Select, scalar: bool = True):
+        response = await self.session.execute(query)
 
         result = response.scalar() if scalar else response
 
         return result
 
-    def update(
+    async def update(
             self,
             data: dict,
             pk: Any,
@@ -48,17 +48,17 @@ class BaseRepository:
             .returning(self.model)
         )
 
-        response = self.session.execute(query, data)
+        response = await self.session.execute(query, data)
 
         if not in_session:
-            self.session.commit()
+            await self.session.commit()
 
         result = response.one()
 
         return result
 
-    def delete(self, obj_id: int):
+    async def delete(self, obj_id: int):
         query = delete(self.model).where(self.model.id == obj_id)
 
-        self.session.execute(query)
-        self.session.commit()
+        await self.session.execute(query)
+        await self.session.commit()

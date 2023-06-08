9ec0400deaa6da0fd767514419399912f0182139
manjustice <manjustice1998@gmail.com>
2023-06-08 16:03:44 +0300

    Added main serializers for cafe

--- app/serializers/cafe.py
+++ app/serializers/cafe.py
@@ -1,11 +1,39 @@
+from datetime import time
+
 from pydantic import BaseModel
+
+from app.models import AverageBill
+
+
+class ImageBase(BaseModel):
+    url: str
+
+    class Config:
+        orm_mode = True
 
 
 class CafeBase(BaseModel):
     name: str
 
 
-class Cafe(CafeBase):
+class CafeList(CafeBase):
+    id: int
+    places: int
+    images: list[ImageBase] = []
+    rating: float
+    reviews: int
+    average_bill: AverageBill
 
     class Config:
         orm_mode = True
+
+
+class Cafe(CafeList):
+    street: str
+    work_time_start: time
+    work_time_end: time
+    has_wifi: bool
+    has_coworking_place: bool
+    can_with_pets: bool
+    has_outdoor_seating: bool
+    has_vegan_menu: bool

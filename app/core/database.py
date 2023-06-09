88984a7c422fa5025dd160dee2579b5f4cc47359
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-06-09 16:25:29 +0300

    Implement FastAPI User model

--- app/core/database.py
+++ app/core/database.py
@@ -1,8 +1,10 @@
 from typing import Any
 
+from fastapi_users_db_sqlalchemy import SQLAlchemyBaseUserTable
+from sqlalchemy import Column, Integer, String
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./cafe.db"
 
@@ -13,3 +15,12 @@ async_session = sessionmaker(
     engine, class_=AsyncSession, expire_on_commit=False
 )
 Base: Any = declarative_base()
+
+
+class User(SQLAlchemyBaseUserTable[int], Base):
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True)
+    hashed_password = Column(String)
+    first_name = Column(String)
+    last_name = Column(String)
+    orders = relationship("Order", back_populates="user")
--- app/models/user.py
+++ app/models/user.py
@@ -1,5 +1,6 @@
-from sqlalchemy import Column, Integer, String
+from sqlalchemy import Column, Integer, String, Boolean
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import relationship
 
 Base = declarative_base()
 
@@ -8,7 +9,11 @@ class User(Base):
     __tablename__ = "user"
 
     id = Column(Integer, primary_key=True, index=True)
-    email = Column(String, unique=True, index=True)
-    hashed_password = Column(String)
-    first_name = Column(String)
-    last_name = Column(String)
+    email = Column(String, unique=True, index=True,  nullable=False)
+    hashed_password = Column(String,   nullable=False)
+    first_name = Column(String,  nullable=False)
+    last_name = Column(String,  nullable=False)
+    is_active = Column(Boolean,  default=True)
+    is_superuser = Column(Boolean, default=False)
+    is_verified = Column(Boolean,  default=False)
+    orders = relationship("Order", back_populates="user")  # orders will be created further

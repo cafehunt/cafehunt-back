d741bab7ab486189b4b34a0952d04d3164c765d7
manjustice <manjustice1998@gmail.com>
2023-06-14 16:50:25 +0300

    Created endpoints for order

--- app/api/order.py
+++ app/api/order.py
@@ -1,14 +1,39 @@
-from fastapi import APIRouter, HTTPException
+from fastapi import APIRouter
 from fastapi.params import Depends
-from starlette import status
+from app.api.user import fastapi_users
 
-from app.serializers.cafe import Cafe, CafeList
-from app.services.cafe import CafeService
-from app.utils.dependencies.services import get_cafe_service
+from app.models import User
+from app.services.order import OrderService
+from app.serializers.order import OrderCreate, Order
+from app.utils.dependencies.services import get_order_service
 
 router = APIRouter()
 
 
-@router.get("", response_model=None)
-async def get_user_orders():
-    pass
+@router.get("", response_model=list[Order])
+async def get_user_orders(
+        user: User = Depends(fastapi_users.current_user()),
+        service: OrderService = Depends(get_order_service)
+):
+    return await service.get_user_orders(user=user)
+
+
+@router.post("", response_model=Order)
+async def create_order(
+        order_data: OrderCreate,
+        user: User = Depends(fastapi_users.current_user()),
+        service: OrderService = Depends(get_order_service)
+):
+    order_data_dict = order_data.dict()
+    order_data_dict["user_id"] = user.id
+
+    return await service.create_order(data=order_data_dict)
+
+
+@router.delete("/{order_id}/")
+async def delete_order(
+        order_id: int,
+        user: User = Depends(fastapi_users.current_user()),
+        service: OrderService = Depends(get_order_service)
+):
+    return await service.delete_order(order_id, user)

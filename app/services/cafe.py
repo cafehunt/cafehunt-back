160523326dc537f7d97fcf9f56e93ce6ca86cf99
manjustice <manjustice1998@gmail.com>
2023-06-14 16:50:46 +0300

    Added get_vacant_places

--- app/services/cafe.py
+++ app/services/cafe.py
@@ -1,6 +1,7 @@
-from sqlalchemy import select
+from sqlalchemy import select, func, and_
 from sqlalchemy.orm import joinedload
 
+from app.models import Order
 from app.repositories.cafe_repo import CafeRepository
 from app.serializers.cafe import Cafe
 from app.models.cafe import Cafe
@@ -11,18 +12,32 @@ class CafeService:
     def __init__(self, cafe_repo: CafeRepository):
         self.repo = cafe_repo
 
-    def get_all_cafes(self):
+    async def get_all_cafes(self):
         query = (
             select(Cafe).join(Cafe.images)
             .options(joinedload(Cafe.images))
         )
-        return self.repo.get_all(query)
+        return await self.repo.get_all(query)
 
     async def get_cafe_by_id(self, cafe_id):
         query = (
             select(Cafe).join(Cafe.images)
             .options(joinedload(Cafe.images))
             .where(Cafe.id == cafe_id)
+        )
+
+        return await self.repo.get_one_obj(query)
+
+    async def get_vacant_places(self, cafe_id: int, date: str):
+        query = select(
+            Cafe.id,
+            (Cafe.places - func.coalesce(func.sum(Order.places), 0))
+            .label("available_places")
+        ).join(Order).where(
+            and_(
+                Order.cafe_id == cafe_id,
+                Order.booking_date == date
+            )
         )
 
         return await self.repo.get_one_obj(query)

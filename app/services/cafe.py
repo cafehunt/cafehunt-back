7905f32eaf19ced03fbb2d6dcb2813913c69ee92
manjustice <manjustice1998@gmail.com>
2023-06-15 17:29:51 +0300

    Updated get_cafe_by_id added validate_date

--- app/services/cafe.py
+++ app/services/cafe.py
@@ -1,5 +1,9 @@
+from datetime import datetime
+
+from fastapi import HTTPException
 from sqlalchemy import select, func, and_
 from sqlalchemy.orm import joinedload
+from starlette import status
 
 from app.models import Order
 from app.repositories.cafe_repo import CafeRepository
@@ -19,25 +23,58 @@ class CafeService:
         )
         return await self.repo.get_all(query)
 
-    async def get_cafe_by_id(self, cafe_id):
-        query = (
-            select(Cafe).join(Cafe.images)
-            .options(joinedload(Cafe.images))
-            .where(Cafe.id == cafe_id)
-        )
+    async def get_cafe_by_id(self, cafe_id: int, with_images: bool = True):
+        query = select(Cafe).where(Cafe.id == cafe_id)
+
+        if with_images:
+            query = query.options(joinedload(Cafe.images))
 
         return await self.repo.get_one_obj(query)
 
     async def get_vacant_places(self, cafe_id: int, date: str):
-        query = select(
-            Cafe.id,
-            (Cafe.places - func.coalesce(func.sum(Order.places), 0))
-            .label("available_places")
-        ).join(Order).where(
-            and_(
-                Order.cafe_id == cafe_id,
-                Order.booking_date == date
+        cafe = await self.get_cafe_by_id(cafe_id, with_images=False)
+
+        if cafe is None:
+            raise HTTPException(
+                status_code=status.HTTP_404_NOT_FOUND,
+                detail="Not found cafe"
+            )
+
+        date = await self.validate_date(date)
+
+        query = (
+            select(func.sum(Order.places))
+            .where(
+                and_(
+                    Order.cafe_id == cafe_id,
+                    Order.booking_date == date  # Change it after moving to PostgreSQL
+                )
             )
         )
 
-        return await self.repo.get_one_obj(query)
+        booked_places = await self.repo.get_one_obj(query)
+
+        if booked_places is None:
+            available_places = cafe.places
+        else:
+            available_places = cafe.places - booked_places
+
+        return {"cafe_id": cafe_id, "available_places": available_places}
+
+    @staticmethod
+    async def validate_date(date: str) -> datetime | HTTPException:
+        try:
+            date = datetime.strptime(date, "%Y.%m.%d %H")
+        except ValueError:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Invalid date format. Expected format: YYYY.MM.DD HH"
+            )
+
+        if datetime.now() > date:
+            raise HTTPException(
+                status_code=status.HTTP_400_BAD_REQUEST,
+                detail="Booking date cannot be in the past"
+            )
+
+        return date

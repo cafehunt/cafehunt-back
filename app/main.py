0dfbf174c30521458fb84bca56c274572a8a1cd1
Yevhen Kopatko <cs.kopatko.evgen@gmail.com>
2023-06-28 16:27:18 +0300

    Fix CORS error

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 
 from app.api import api_router
 from app.core.database import engine, Base
@@ -6,6 +7,20 @@ from fastapi_pagination import add_pagination
 
 app = FastAPI()
 
+origins = [
+    "http://127.0.0.1:5176",
+    "http://localhost:5176"
+    # "productiondomain.com"
+]
+
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(api_router)
 add_pagination(app)

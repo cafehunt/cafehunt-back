51cd22a74f0b921334b620445eed05467a378003
manjustice <manjustice1998@gmail.com>
2023-06-29 21:34:12 +0300

    sqlalchemy set variable in env.py

--- app/core/database.py
+++ app/core/database.py
@@ -4,7 +4,7 @@ from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite+aiosqlite:///./cafe.db"
+from config import SQLALCHEMY_DATABASE_URL
 
 
 engine = create_async_engine(SQLALCHEMY_DATABASE_URL, echo=True, future=True)
